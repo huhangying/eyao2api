2f8c887bea771c46877a1665eea3f8c758d51f71
Harry Hu <harry.hu@yahoo.ca>
2020-04-25 11:33:05 -0400

    remove debug

--- db/model/doctor.js
+++ db/model/doctor.js
@@ -1,36 +1,37 @@
 /**
  * Created by hhu on 2016/5/7.
  */
-var _Doctor = new Schema({
-  hid: { type: String },
-  token: { type: String }, // token should not store into db
-    user_id: {type: String, required: true},
-    password: {type: String, required: true},
-    name: {type: String, required: true},
-    role: {type: Number, required: true, default: 0 },
-    department: {type: Schema.Types.ObjectId, ref: 'department', required: true },
+module.exports = mongoose.model(
+  'doctor',
+  new Schema({
+    hid: { type: String },
+    token: { type: String }, // token should not store into db
+    user_id: { type: String, required: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    role: { type: Number, required: true, default: 0 },
+    department: { type: Schema.Types.ObjectId, ref: 'department', required: true },
     title: String,
     tel: String,
     cell: String,
     gender: String,
     hours: String,
-    expertise:  String,
+    expertise: String,
     bulletin: String,
     honor: String,
     icon: String,
     status: { type: Number, default: 0, min: 0, max: 3 },  // 0: idle, 1: busy; 2: away; 3: offline
     shortcuts: { type: String }, // 快捷回复, separated by '|'
-  // associates: [ // NO USE now, in case one doctor has multiple positions in hospitals.
-  //   {
-  //     hid: { type: String },
-  //     hdid: { type: String }
-  //   }
-  // ],
-    created: {type : Date, default: Date.now },
-    updated: {type : Date, default: Date.now },
+    // associates: [ // NO USE now, in case one doctor has multiple positions in hospitals.
+    //   {
+    //     hid: { type: String },
+    //     hdid: { type: String }
+    //   }
+    // ],
+    created: { type: Date, default: Date.now },
+    updated: { type: Date, default: Date.now },
     locked_count: Number,
-    apply: {type : Boolean, default: true },
+    apply: { type: Boolean, default: true },
     order: { type: Number, default: 0 }
-});
-
-module.exports =  mongoose.model('doctor', _Doctor);
+  })
+);
--- index.js
+++ index.js
@@ -1,36 +1,57 @@
-#!/usr/bin/env node
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
-/**
- * Module dependencies.
- */
+// 公用库
+global.Status = require('./util/status.js');
+global.mongoose = require('mongoose');
+global.Schema = global.mongoose.Schema;
+global.moment = require('moment');
+global.Consts = require('./util/consts.js');
+global.mongoose.connect('mongodb://192.168.87.250/eyao', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+ });
 
-const app = require('./app');
-const debug = require('debug')('eyao2api:server');
-const http = require('http');
+const routes = require('./routes/index');
 
-/**
- * Create HTTP server.
- */
+const app = express();
 
-const server = http.createServer(app);
+//设置跨域访问
+app.use(cors());
+app.all('*', function (req, res, next) {
+  res.header("X-Powered-By", 'hwem')
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
 
-/**
- * Listen on provided port, on all network interfaces.
- */
+// view engine setup
+app.set('views', path.join(path.resolve(), 'views'));
+app.set('view engine', 'ejs');
+app.set('view options', {
+  layout: false
+});
+app.disable('etag'); //avoid 304 error
 
-server.listen(3000, () => {
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(path.resolve(), 'public')));
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use((req, res) => {
+  res.sendStatus(404);
+});
+
+app.listen(3000, () => {
   console.log('Server running at 3000 port.');
 });
-server.on('listening', onListening);
 
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  let addr = server.address();
-  let bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
+module.exports = app;

f410c792bc9c54d0106dffd12bab986093039bca
Harry Hu <harry.hu@yahoo.ca>
2020-04-28 23:25:55 -0400

    refactor group

--- db/controller/group.js
+++ db/controller/group.js
@@ -63,62 +63,14 @@ module.exports = {
             .catch(err => next(err));
     },
 
-    UpdateById: function (req, res) {
+    UpdateById: (req, res, next) => {
         const { id } = req.params;// params.id is group ID
 
         // 获取数据（json）,只能更新关系组名
-        var group = req.body;
-        var group_name;
-
-        Group.findById(id, function (err, item) {
-            if (err) {
-                return Status.returnStatus(res, Status.ERROR, err);
-            }
-
-            if (!item) {
-                return Status.returnStatus(res, Status.NULL);
-            }
-            group_name = item.name; // 原id的group name
-
-            if (group.doctor)
-                item.doctor = group.doctor;
-            if (group.name) {
-                item.name = group.name;
-            }
-            if (group.apply || group.apply === false)
-                item.apply = group.apply;
-
-            // 如果group name 没有变,或者group name不需要更新,则不用check duplicated group name
-            if (group_name == item.name || !group.name) {
-                item.save(function (err, raw) {
-                    if (err) {
-                        return Status.returnStatus(res, Status.ERROR, err);
-                    }
-                    return res.json(raw);
-                });
-            }
-
-            // check if duplication group name in a doctor
-            Group.find({ name: group.name, doctor: group.doctor }) // check if existed
-                .exec(function (err, items) {
-                    if (err) {
-                        return Status.returnStatus(res, Status.ERROR, err);
-                    }
-
-                    // 如果存在，直接返回
-                    if (items && items.length > 0 && items[0].name != group_name) {
-                        return Status.returnStatus(res, Status.EXISTED_NAME);
-                    }
-
-                    item.save(function (err, raw) {
-                        if (err) {
-                            return Status.returnStatus(res, Status.ERROR, err);
-                        }
-                        res.json(raw);
-                    });
-                });
-        });
-
+        const group = { ...req.body };
+        Group.findByIdAndUpdate(id, group, { new: true })
+          .then((result) => res.json(result))
+          .catch(err => next(err));
     },
 
 

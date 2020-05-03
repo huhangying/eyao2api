8adfd987cef25b9ee207af9a8857305c9dc7d58c
Harry Hu <harry.hu@yahoo.ca>
2020-05-02 23:21:07 -0400

    refactor medicine

--- db/controller/medicine.js
+++ db/controller/medicine.js
@@ -1,179 +1,74 @@
 /**
  * Created by harry on 16/10/2.
  */
-var Medicine = require('../model/medicine.js');
+const Medicine = require('../model/medicine.js');
 
 module.exports = {
 
-    GetAll: function (req, res) {
-
-        Medicine.find()
-            .exec(function (err, items) {
-                if (err) {
-                    return Status.returnStatus(res, Status.ERROR, err);
-                }
-
-                if (!items || items.length < 1) {
-                    return Status.returnStatus(res, Status.NULL);
-                }
-
-                res.json(items);
-            });
-    },
-
-    // 根据ID获取详细信息
-    GetById: function (req, res) {
-
-        if (req.params && req.params.id) {
-
-            Medicine.findOne({_id: req.params.id})
-                .exec(function (err, item) {
-                    if (err) {
-                        return Status.returnStatus(res, Status.ERROR, err);
-                    }
-
-                    if (!item) {
-                        return Status.returnStatus(res, Status.NULL);
-                    }
-
-                    res.json(item);
-                });
-        }
+    GetAll: (req, res, next) => {
+        Medicine.find({ hid: req.token.hid })
+            .select('-hid -__v')
+            .lean()
+            .then((result) => res.json(result))
+            .catch(err => next(err));
     },
 
     // 获取所有药的列表, 用于 autoComplete 功能
-    GetAllAvailable: function (req, res) {
+    GetAllAvailable: (req, res, next) => {
+        Medicine.find({ hid: req.token.hid })
+            .select('-hid -__v')
+            .lean()
+            .then((result) => res.json(result))
+            .catch(err => next(err));
+    },
 
-        Medicine.find({apply: true}
-        //todo:
-        )
-            .exec(function (err, items) {
-                if (err) {
-                    return Status.returnStatus(res, Status.ERROR, err);
-                }
-
-                if (!items || items.length < 1) {
-                    return Status.returnStatus(res, Status.NULL);
-                }
-
-                res.json(items);
-            });
+    // 根据ID获取详细信息
+    GetById: (req, res, next) => {
+        const { id } = req.params;
+        Medicine.findOne({ _id: id, apply: true })
+            .select('-hid -__v')
+            .then((result) => res.json(result))
+            .catch(err => next(err));
     },
 
     // 创建
-    Add: function (req, res) {
-
-        // 获取请求数据（json）
-        var medicine = req.body;
-
+    Add: (req, res, next) => {
+        const medicine = req.body;
         // name
         if (!medicine.name) {
             return Status.returnStatus(res, Status.NO_NAME);
         }
-        // unit
-        // if (!medicine.unit) {
-        //     return Status.returnStatus(res, Status.MISSING_PARAM);
-        // }
+        // unit, dosage, category
+        if (!medicine.unit || !medicine.usage || !medicine.capacity) {
+            return Status.returnStatus(res, Status.MISSING_PARAM);
+        }
 
-        // dosage
-        // if (!medicine.dosage) {
-        //     return Status.returnStatus(res, Status.MISSING_PARAM);
-        // }
-
-        // category
-        // if (!medicine.cat) {
-        //     return Status.returnStatus(res, Status.NO_CAT);
-        // }
-
-
-        // 不存在，创建
-        Medicine.create({
-            hid: medicine.hid,
-            name: medicine.name,
-            desc: medicine.desc,
-            unit: medicine.unit,
-            capacity: medicine.capacity,
-            usage: medicine.usage,
-            dosage: medicine.dosage,
-            //cat: medicine.cat,
-            notices: medicine.notices
-        }, function (err, raw) {
-            if (err) {
-                return Status.returnStatus(res, Status.ERROR, err);
-            }
-            return res.send(raw);
-        });
-
+        Medicine.exists({ name: medicine.name, hid: medicine.hid }) // check if existed
+        .then(result => {
+          if (result) return Status.returnStatus(res, Status.EXISTED);
+  
+          Medicine.create(medicine)
+            .then((result) => res.json(result))
+            .catch(err => next(err));
+        })
+        .catch(err => next(err));
     },
 
-    UpdateById: function (req, res) {
-        if (req.params && req.params.id) { // params.id is ID
-            var id = req.params.id;
-
-            // 获取数据（json）
-            var medicine = req.body;
-
-            Medicine.findById(id, function (err, item) {
-                if (err) {
-                    return Status.returnStatus(res, Status.ERROR, err);
-                }
-
-                if (!item) {
-                    return Status.returnStatus(res, Status.NULL);
-                }
-
-                if (medicine.name)
-                    item.name = medicine.name;
-                if (medicine.desc)
-                    item.desc = medicine.desc;
-                if (medicine.unit)
-                    item.unit = medicine.unit;
-                if (medicine.capacity)
-                    item.capacity = medicine.capacity;
-                if (medicine.usage)
-                    item.usage = medicine.usage;
-                if (medicine.dosage)
-                    item.dosage = medicine.dosage;
-                if (medicine.notices)
-                    item.notices = medicine.notices;
-                if (medicine.apply || medicine.apply === false)
-                    item.apply = medicine.apply;
-
-                //
-                item.save(function (err, raw) {
-                    if (err) {
-                        return Status.returnStatus(res, Status.ERROR, err);
-                    }
-                    res.json(raw);
-                });
-
-            });
-
-        }
+    UpdateById: (req, res, next) => {
+        const { id } = req.params;
+        const medicine = { ...req.body };
+        Medicine.findByIdAndUpdate(id, medicine, { new: true })
+            .select('-hid -__v')
+            .then((result) => res.json(result))
+            .catch(err => next(err));
     },
 
-    DeleteById: function (req, res) {
-        if (req.params && req.params.id) { // params.id is group ID
-
-            Medicine.findOne({_id: req.params.id}, function (err, item) {
-                if (err) {
-                    return Status.returnStatus(res, Status.ERROR, err);
-                }
-
-                if (!item){
-                    return Status.returnStatus(res, Status.NULL);
-                }
-
-                //
-                item.remove(function(err, raw){
-                    if (err) {
-                        return Status.returnStatus(res, Status.ERROR, err);
-                    }
-                    res.json(raw);
-                });
-
-            });
-        }
+    DeleteById: async (req, res, next) => {
+        const { id } = req.params;
+        Medicine.findByIdAndDelete(id)
+            .select('-hid -__v')
+            .then((result) => res.json(result))
+            .catch(err => next(err));
     },
 
 }

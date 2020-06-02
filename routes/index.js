e1af808c36abaaab629f33c18ab9d62bc61b9bfa
Harry Hu <harry.hu@yahoo.ca>
2020-06-02 12:59:05 -0400

    test

--- routes/index.js
+++ routes/index.js
@@ -737,8 +737,8 @@ const wechat = require('../wechat/sign');
 router.route('/wechat/signature')
     .get(wechat.signatureTest)
     .post(wechat.signatureAuth);
-router.route('/wechat/checkSignature')
-    .get(wechat.checkSignature);
+router.route('/wechat/getSignature')
+    .get(wechat.getSignature);
 
-    
+
 module.exports = router;
--- wechat/sign.js
+++ wechat/sign.js
@@ -1,50 +1,44 @@
 const TOKEN = 'harry';
 const Wechat = require('wechat-jssdk');
-
 const wxConfig = {
     //set your oauth redirect url, defaults to localhost
-    "wechatRedirectUrl": "http://timebox.i234.me/wechat/oauth-callback",
+    // "wechatRedirectUrl": "http://timebox.i234.me/wechat/",
     //"wechatToken": "wechat_token", //not necessary required
     "appId": 'wxac12d83affdb4dd5',
     "appSecret": 'a6cdf7e9c01039d03f3255cf5826a189',
-    // card: true, //enable cards
-    // payment: true, //enable payment support
-    // merchantId: '', //
-    // paymentSandBox: true, //dev env
-    // paymentKey: '', //API key to gen payment sign
-    // paymentCertificatePfx: fs.readFileSync(path.join(process.cwd(), 'cert/apiclient_cert.p12')),
-    //default payment notify url
-    // paymentNotifyUrl: `http://your.domain.com/api/wechat/payment/`,
 }
+
 
 
 module.exports = {
     // wechat sign test
     signatureTest: (req, res) => {
-        const { signature, timestamp, nonce, echostr } = req.query;
-        // console.log(signature, timestamp, nonce, echostr);
-        res.send(echostr);
+        // res.send(req.query.echostr);
+        const wx = new Wechat(wxConfig);
+        if (wx.jssdk.verifySignature(req.query)) {
+            res.send(req.query.echostr);
+            return;
+        }
+        res.send("error");
     },
 
     signatureAuth: (req, res) => {
         const { signature, timestamp, nonce, openid } = req.query;
-        console.log(signature, timestamp, nonce, openid);
+        console.log(req.query);
 
-        const wx = new Wechat(wxConfig);
-        //const url = wx.oauth.generateOAuthUrl('http://timebox.i234.me/wechat/', 'snsapi_base', '101');
+        // const wx = new Wechat(wxConfig);
+        // const url = wx.oauth.generateOAuthUrl('http://timebox.i234.me/wechat/', 'snsapi_base', '101');
         //console.log(url);
         const url = 'http://timebox.i234.me/wechat/?openid=' + openid;
-        
-        res.render("oauth-page", {
-            wechatOAuthUrl: url,
-        });
-        // res.redirect('/wechat/?openid=' + openid);
-        // res.send(openid);
+
+        // res.render("oauth-page", {
+        //     wechatOAuthUrl: url,
+        // });
     },
 
-    checkSignature: (req, res) => {
-        const wx = new Wechat(wxConfig);
+    getSignature: (req, res) => {
         try {
+            const wx = new Wechat(wxConfig);
             wx.jssdk.getSignature(req.query.url).then(signatureData => {
                 res.json(signatureData);
             });

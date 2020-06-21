1c8c4e53befc2874a12d7fd91f0cf791b46dfebf
Harry Hu <harry.hu@yahoo.ca>
2020-06-21 12:21:33 -0400

    test

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ global.Status = require('./util/status.js');
 global.urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const app = express();
+//设置跨域访问
+app.use(cors());
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 require('./io/socketio')(io);
 
-//设置跨域访问
-app.use(cors());
 app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");

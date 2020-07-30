a59c223f7d4007d758f90b915c09acf4381010ff
Harry Hu <harry.hu@yahoo.ca>
2020-07-29 20:42:06 -0400

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/eyao', {
+mongoose.connect('mongodb://192.168.87.250/eyao', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,

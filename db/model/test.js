9e5a731e7f47e14d3b2ff01fc41d21bfc85383b5
Harry Hu <harry.hu@yahoo.ca>
2020-08-23 11:28:08 -0400

    reverse testform and add test data model

--- db/model/test.js
+++ db/model/test.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-// 化验单模板
+// 化验单
 module.exports = mongoose.model(
-  'test_form',
+  'test',
   mongoose.Schema({
     hid: Number,
     name: { type: String, required: true, trim: true },
@@ -14,18 +14,17 @@ module.exports = mongoose.model(
         code: { type: String },
         unit: { type: String },
         reference: { type: String },
-        riskValues: [
-          {
-            value: { type: Number, min: -3, max: 3 },
-            name: { type: String }, // optional
-            from: { type: Number },
-            to: { type: Number },
-          }
-        ]
+        
+        result: { type: String, trim: true }, 
+        riskValue: {
+          value: { type: Number, min: -3, max: 3 },
+          name: { type: String }, // optional
+          from: { type: Number },
+          to: { type: Number },
+        }
       }
     ],
-    order: { type: Number },
-    apply: { type: Boolean, default: true }
+    // order: { type: Number },
   }, {
     timestamps: true
   })

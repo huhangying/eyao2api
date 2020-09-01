7a93745a9ce8578a09a2aaae44302703c85336fc
Harry Hu <harry.hu@yahoo.ca>
2020-08-31 23:49:43 -0400

    test add user&doctor

--- db/model/test.js
+++ db/model/test.js
@@ -5,6 +5,9 @@ module.exports = mongoose.model(
   'test',
   mongoose.Schema({
     hid: Number,
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+    doctor: { type: mongoose.Schema.Types.ObjectId, ref: 'doctor' },
+    
     name: { type: String, required: true, trim: true },
     type: { type: String },
 
@@ -14,8 +17,8 @@ module.exports = mongoose.model(
         code: { type: String },
         unit: { type: String },
         reference: { type: String }, // risk value = 0
-        
-        result: { type: Number, trim: true }, 
+
+        result: { type: Number, trim: true },
         riskValues: [{
           value: { type: Number, min: -3, max: 3 },
           name: { type: String }, // optional

9a4d1e0b158003fead2614f088bbc2f9b34bea15
Harry Hu <harry.hu@yahoo.ca>
2020-09-02 20:51:37 -0400

    add default role=0 when subscribe

--- wechat/message-handler.js
+++ wechat/message-handler.js
@@ -120,6 +120,7 @@ const subscribe = async (baseData, did) => {
         name: userInfo.data.nickname,
         gender: gender,
         icon: userInfo.data.headimgurl,
+        role: 0,
         apply: true,
         created: new Date(),
         updated: new Date()

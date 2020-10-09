f8835f3cbab00a3df30762545fb1224b23487b7e
Harry Hu <harry.hu@yahoo.ca>
2020-10-08 20:13:42 -0400

    add package q back

--- db/model/relationship.js
+++ db/model/relationship.js
@@ -1,6 +1,5 @@
 
 const mongoose = require('mongoose');
-// const Q = require('q');
 
 const Relationship = mongoose.model(
     'relationship',
@@ -14,67 +13,5 @@ const Relationship = mongoose.model(
         timestamps: true
     })
 );
-
-
-/**
- * @description
- * Returns a function which will sort an
- * array of objects by the given key.
- *
- * @param  {String}  key
- * @param  {Boolean} reverse
- * @return {Function}
- */
-// function sortBy(key, reverse) {
-
-//     // Move smaller items towards the front
-//     // or back of the array depending on if
-//     // we want to sort the array in reverse
-//     // order or not.
-//     var moveSmaller = reverse ? 1 : -1;
-
-//     // Move larger items towards the front
-//     // or back of the array depending on if
-//     // we want to sort the array in reverse
-//     // order or not.
-//     var moveLarger = reverse ? -1 : 1;
-
-//     /**
-//      * @param  {*} a
-//      * @param  {*} b
-//      * @return {Number}
-//      */
-//     return function (a, b) {
-//         if (a[key] < b[key]) {
-//             return moveSmaller;
-//         }
-//         if (a[key] > b[key]) {
-//             return moveLarger;
-//         }
-//         return 0;
-//     };
-
-// }
-
-// Relationship.getFocusDoctors = function (userId, hid) {
-//     var deferred = Q.defer();
-
-//     Relationship.find({ user: userId, hid: hid, apply: true })
-//         .exec(function (err, items) {
-//             if (err) {
-//                 deferred.resolve([]);
-//             }
-
-//             if (!items || items.length < 1) {
-//                 deferred.resolve([]);
-
-//             }
-
-//             var doctors = items.map(function (a) { return a.doctor; }).sort(sortBy('order'));
-//             deferred.resolve(doctors);
-//         });
-
-//     return deferred.promise;
-// };
 
 module.exports = Relationship;

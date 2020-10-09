af6bf42d5f0168a7b704aa94d9c8cc3ba1a6124a
Harry Hu <harry.hu@yahoo.ca>
2020-10-08 21:26:57 -0400

    add socket-io listening booking

--- io/socketio.js
+++ io/socketio.js
@@ -53,5 +53,19 @@ module.exports = (io) => {
                 created: feedback.createdAt
             });
         });
+
+        // Booking (cancel for now)
+
+        socket.on('booking', (room, booking) => {
+            
+            // redirect to noti
+            socket.to(room).emit('notification', {
+                patientId: booking.user._id,
+                type: 3,
+                name: `${booking.user.name} 取消了${moment(booking.date).format('LL')}预约`,
+                count: 1,
+                created: booking.created
+            });
+        });
     });
 }

311af9dac32c33260477a735335444c334127164
Harry Hu <harry.hu@yahoo.ca>
2020-10-08 23:17:55 -0400

    comment

--- db/model/booking.js
+++ db/model/booking.js
@@ -9,7 +9,8 @@ module.exports = mongoose.model(
         schedule: { type: mongoose.Schema.Types.ObjectId, ref: 'schedule', required: true }, // 预约
         date: { type: Date }, // for 搜索
         status: { type: Number, default: 0 }, // 1: 预约完成,可用状态 2: user cancel; 3: doctor cancel;
-                                              //  4: pending (药师接手未完成);  5: finished  6. confirm finished
+                                              // 4: pending (药师前转未完成);  5: finished  6. confirm finished
+                                              // 7: forwarded pending （药师接手未完成）
         score: { type: Number, min: 0, max: 10, default: 0 },//0: not started yet.
         notes: String,
         created: { type: Date, default: Date.now }

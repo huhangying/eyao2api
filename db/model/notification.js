5e52d36c2b1b617f5766e17b693286d9bca0a57b
Harry Hu <harry.hu@yahoo.ca>
2020-10-18 12:47:41 -0400

    add consult noti supporting

--- db/model/notification.js
+++ db/model/notification.js
@@ -10,7 +10,7 @@ module.exports = mongoose.model(
         userName: String,
         icon: String,
         direction: { type: Boolean, required: true },  // true: user -> doctor; false: doctor -> user
-        // 類別： 0：chat   1: 不良反应   2: 联合用药   3. 预约取消   4. 付费咨询
+        // 類別： 0：chat  1: 不良反应  2: 联合用药  3. 预约取消  4. 客服chat  5. 付费图文咨询  6. 付费电话咨询
         type: { type: Number, required: true }, 
         count: { type: Number, default: 0 }
     }, {
--- io/socketio.js
+++ io/socketio.js
@@ -66,7 +66,7 @@ module.exports = (io) => {
             });
         });
 
-        // Booking (cancel for now)
+        // Booking (cancel feature for now)
 
         socket.on('booking', (room, booking) => {
             // console.log('---> on booking: ', room, booking);
@@ -77,6 +77,20 @@ module.exports = (io) => {
                 name: `${booking.user.name} 取消了${moment(booking.date).format('LL')}预约`,
                 count: 1,
                 created: booking.created
+            });
+        });
+
+        // Consult
+
+        socket.on('consult', (room, consult) => {
+            // console.log('---> on booking: ', room, booking);
+            // redirect to noti
+            socket.to(room).emit('notification', {
+                patientId: consult.user,
+                type: consult.type === 1 ? 6 : 5, // 付费咨询 (0: 图文；1：电话) => notiType (5: 图文；6：电话 )
+                name: `${consult.userName} 发送了付费${consult.type === 1 ? '电话' : '图文'}咨询`,
+                count: 1,
+                created: consult.created
             });
         });
     });

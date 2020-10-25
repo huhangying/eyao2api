e0a67fa8779354db0c984e6b75ada240501ae14f
Harry Hu <harry.hu@yahoo.ca>
2020-10-24 22:04:41 -0400

    test

--- wechat/auth.service.js
+++ wechat/auth.service.js
@@ -161,7 +161,8 @@ const resendFailedMsg = async (req, res, next) => {
 						successCount++;
 					} else {
 						// failed and increase tryCount
-						await wxMsgQueue.findByIdAndUpdate(msg._id, { $inc: { tryCount: 1 } }).exec();
+						await wxMsgQueue.findByIdAndUpdate(msg._id, { $inc: { tryCount: 1 } }).then(() => {
+						});
 						failedCount++;
 					}
 				}

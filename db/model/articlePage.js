c571ed47e7f4a67fcf0ee690f82cfba821f253a6
Harry Hu <harry.hu@yahoo.ca>
2020-12-27 15:38:45 -0500

    change page ref

--- db/model/articlePage.js
+++ db/model/articlePage.js
@@ -7,7 +7,7 @@ module.exports = mongoose.model(
         hid: Number,
         doctor: { type: mongoose.Schema.Types.ObjectId, ref: 'doctor', required: true },  // from
         doctor_name: String,
-        cat: { type: mongoose.Schema.Types.ObjectId, ref: 'page_cat', required: true }, // remove?
+        cat: { type: mongoose.Schema.Types.ObjectId, ref: 'article_cat', required: true }, 
         name: { type: String, required: true, trim: true }, // page section name
         title: { type: String, required: true },
         title_image: { type: String },

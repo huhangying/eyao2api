14b3dfa12b7e28a46165fbed396cbf0f1aafc7da
Harry Hu <harry.hu@yahoo.ca>
2020-12-31 01:00:32 -0500

    notificateion: use doctorid instead of sender

--- io/socketio.js
+++ io/socketio.js
@@ -10,12 +10,12 @@ module.exports = (io) => {
 
         socket.on('joinRoom', (room) => {
             socket.join(room);
-            console.log('joined to room ' + room);
+            // console.log('joined to room ' + room);
         });
 
         socket.on('leaveRoom', (room) => {
             socket.leave(room);
-            console.log('left room ' + room);
+            // console.log('left room ' + room);
         });
 
         // Chat
@@ -25,7 +25,7 @@ module.exports = (io) => {
             socket.to(room).emit('chat', chat);
 
             socket.to(room).emit('notification', {
-                sender: chat.sender,
+                doctorId: room,
                 patientId: chat.sender,
                 // patientId: room !== chat.sender ? chat.sender: chat.to,
                 type: 0,
@@ -44,7 +44,7 @@ module.exports = (io) => {
             socket.to(room).emit('chat', chat); // 还是用原来chat的那一套来传递消息
 
             socket.to(room).emit('notification', {
-                sender: chat.sender,
+                doctorId: room,
                 patientId: chat.sender,
                 // patientId: room !== chat.sender ? chat.sender: chat.to,
                 type: 4, // 客服chat
@@ -63,7 +63,7 @@ module.exports = (io) => {
             socket.to(room).emit('feedback', feedback);
 
             socket.to(room).emit('notification', {
-                sender: feedback.sender,
+                doctorId: room,
                 patientId: feedback.user,
                 type: feedback.type, // 1 不良反应 or 2 联合用药
                 name: feedback.senderName || '',
@@ -79,7 +79,7 @@ module.exports = (io) => {
             // console.log('---> on booking: ', room, booking);
             // redirect to noti
             socket.to(room).emit('notification', {
-                sender: booking.sender,
+                doctorId: room,
                 patientId: booking.user._id,
                 type: 3, // 门诊预约
                 name: `${booking.user.name} 取消了${moment(booking.date).format('LL')}预约`,
@@ -97,7 +97,7 @@ module.exports = (io) => {
 
             // redirect to noti
             socket.to(room).emit('notification', {
-                sender: consult.sender,
+                doctorId: room,
                 patientId: consult.user,
                 type: consult.type === 1 ? 6 : 5, // 付费咨询 (0: 图文；1：电话) => notiType (5: 图文；6：电话 )
                 name: consult.userName || '',

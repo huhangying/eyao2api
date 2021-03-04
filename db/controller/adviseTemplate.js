7f8bcbde243679663d4679f1036440037bfdfd31
Harry Hu <harry.hu@yahoo.ca>
2021-03-03 19:47:26 -0500

    test

--- db/controller/adviseTemplate.js
+++ db/controller/adviseTemplate.js
@@ -23,7 +23,7 @@ module.exports = {
     GetCmsAdviseTemplatesByDepartmentId: (req, res, next) => {
         const { did } = req.params; // did is department
         const searchCriteria = (did === 'none' || !did) ?
-            { deparment: {$not: {$exists: true}}, hid: req.token.hid }  // null: field not exists
+            { deparment: {$not: {$exists: true, $ne: null}}, hid: req.token.hid }  // null: field not exists
             :
             { department: did, hid: req.token.hid }
         AdviseTemplate.find(searchCriteria)
